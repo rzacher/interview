c39eabbcb59f58a1d24a78f56b434ebfccf07168
Robert Zacher <robert.zacher@one.verizon.com>
2016-11-14 16:43:19 -0500

    first checkin

---
+++ amazon/HeapSort.java
@@ -0,0 +1,33 @@
+package amazon;
+
+import java.util.List;
+import java.util.Collection; 
+import java.util.*; 
+
+
+public class HeapSort {
+
+	public static void main(String[] args) {
+		List<Integer> c = new LinkedList<Integer>();
+		c.add(10);
+		c.add(2);
+		c.add(1); 
+		
+		List<Integer> l = heapSort(c);
+		for (Integer val : l) {
+			System.out.println(val);
+		}
+
+	}
+
+	static <E> List<E> heapSort(Collection<E> c) {
+	    Queue<E> queue = new PriorityQueue<E>(c);
+	    List<E> result = new ArrayList<E>();
+
+	    while (!queue.isEmpty())
+	        result.add(queue.remove());
+
+	    return result;
+	}
+}
+
