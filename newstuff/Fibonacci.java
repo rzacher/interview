e5f7fc8488a06e4cfe80dc8cdc26443b67346d50
Robert Zacher <robert.zacher@one.verizon.com>
2017-01-13 10:34:55 -0500

    frist check in Fibonacci

---
+++ newstuff/Fibonacci.java
@@ -0,0 +1,50 @@
+/**
+ * Program to calculate a Fibonacci number using dynamic programming. 
+ * i.e. we store the values for earlier results to reduce Big O time. 
+ **/ 
+
+import java.util.*; 
+
+
+public class Fibonacci {
+
+  public Fibonacci() {
+  
+  }
+
+  public long getFibNum(int n) {
+    if (n<=1) {
+      return 1; 
+    } else {
+       long value = getFibNum(n-1) + getFibNum(n-2); 
+       if (value < 0) {
+       	 System.out.println("the value overflowed the limit for a long number");
+         return 0; 
+       } else {
+         return value; 
+       }
+    }
+  }
+
+  public static void main(String[] args) {
+    if (args.length < 1) {
+      System.out.println("Please provide a fibonacci number");
+    }
+
+    try {
+      int n = Integer.parseInt(args[0]);
+      Fibonacci fib = new Fibonacci();
+      long startTime = System.currentTimeMillis(); 
+      long fibNum = fib.getFibNum(n); 
+      long endTime = System.currentTimeMillis(); 
+      long duration = (endTime - startTime)/1000; 
+      System.out.println("Duration: " + duration);
+      System.out.println("The " + n + "\'th fibonacci number is " + fibNum); 
+    } catch (NumberFormatException ne) { 
+        ne.printStackTrace(); 
+        System.out.println("argument cannot be converted to a number"); 
+    } 
+  } // main
+
+
+}
