d60a62a64b75843accdfa8aef9aa152347be157c
Robert Zacher <robert.zacher@one.verizon.com>
2017-01-17 17:14:50 -0500

    Adding SimpleNode

---
+++ newstuff/SimpleNode.java
@@ -0,0 +1,28 @@
+// Simple node for linkedList exercises
+
+package newstuff;
+
+public class SimpleNode {
+     SimpleNode next = null;
+     int data;
+
+     public void setNext(SimpleNode next) {
+        this.next = next; 
+     }
+
+     public SimpleNode getNext() {
+        return next; 
+     }
+
+     public void setData(int data) {
+       this.data = data; 
+     }
+
+     public int getData() {
+       return data; 
+     }
+
+     public void printNode() {
+       System.out.println("data:" + data);  
+     }
+}
