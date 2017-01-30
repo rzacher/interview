7e9a207c2895f235daece8cf3eff1c2366976d5e
Robert Zacher <bzacher100@gmail.com>
2017-01-30 16:42:18 -0500

    Added some more classes

---
+++ crack/parkinglot/ParkingLot.java
@@ -0,0 +1,15 @@
+import java.util.*
+
+public class ParkingLot {
+
+  HashMap<Integer, Level> levels = new HashMap<>(); 
+ 
+  parkVehicle(Vehicle vehicle) {
+
+     // Walk the levels, starting at the bottom and find matching spots.
+  }
+     
+  retrieveVehicle(Ticket ticket) {
+
+  }
+}
---
+++ crack/parkinglot/Truck.java
@@ -0,0 +1,8 @@
+import java.util.*
+
+public class Truck {
+
+    public Truck() {
+       super(Size.Medium, 2); 
+    }
+}
