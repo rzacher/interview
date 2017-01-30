7d4d6e8cd15bc07f3b239b08e9f54c509d5e52a1
Robert Zacher <robert.zacher@one.verizon.com>
2017-01-30 13:24:17 -0500

    Added parkinglot

---
+++ crack/parkinglot/Level.java
@@ -0,0 +1,13 @@
+import java.util.*
+
+public static int ROWS = 20;
+public static int COLUMNS = 20;
+
+public class Level {
+   HashMap<Integer, Integer, boolean> occupiedSpots = new HashMap<Integer, Integer>(ROWS, COLUMNS);
+    
+
+   
+
+
+}
