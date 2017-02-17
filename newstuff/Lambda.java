8bfaf71004783cbf58663b5579f962f8be593b33
Robert Zacher <robert.zacher@one.verizon.com>
2017-02-17 09:46:46 -0500

    Added subsets and Lambda.java

---
+++ newstuff/Lambda.java
@@ -0,0 +1,35 @@
+package newstuff; 
+
+import java.util.*;
+
+public class Lambda {
+	
+	
+   private int operate(int a, int b, MathOperation mathOperation){
+      return mathOperation.operation(a, b);
+   }
+   
+   interface MathOperation {
+      int operation(int a, int b);
+   }
+   
+   interface GreetingService {
+     void sayMessage(String message); 
+   } 
+
+    public static void main(String[] args) {
+        Lambda tester = new Lambda(); 
+        
+		System.out.println("Running Lambda expressions");
+		
+		MathOperation addition = (int a, int b) -> a + b; 
+		MathOperation multiplication = (int a, int b) -> a * b;
+		
+		System.out.println("10 + 5 = " + tester.operate(10, 5, addition)); 
+		System.out.println("10 * 5 = " + tester.operate(10, 5, multiplication));
+		
+		GreetingService service1 = (message) -> System.out.println("Message: " + message); 
+		service1.sayMessage("Hi Bob"); 
+    }
+}
+
