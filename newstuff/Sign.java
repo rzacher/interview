67f4f4215eddeef9ed56872061d865991f6484ee
Robert Zacher <robert.zacher@one.verizon.com>
2017-04-12 17:31:07 -0400

    Added sign

---
+++ newstuff/Sign.java
@@ -0,0 +1,21 @@
+package newstuff;
+
+import java.util.*;
+import java.lang.*;
+
+
+// Program to test Math.sign method
+public class Sign {
+
+
+  public static void main(String[] args) {
+     int a = 100; 
+     int b = 200;
+     
+     int[] array = {a,b}; 
+     double  sign =  Math.signum((double) (b-a));
+     int index = (int) (0.5 + sign/2);
+     System.out.println(index);
+     System.out.println(array[index]);
+  }
+}
