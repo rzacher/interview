38d15440e833905ee9ea877c1538ce9fa215a684
Robert Zacher <robert.zacher@one.verizon.com>
2018-02-08 17:31:25 -0500

    Working on PrimeNumber

--- newstuff/PrimeNumber.java
+++ newstuff/PrimeNumber.java
@@ -1,8 +1,10 @@
-package com.newstuff;
+package newstuff;
 
 // Use the sieve of eratosthenes. 
 // Find the list of primes less than max
 public class PrimeNumber {
+	 
+	 boolean[] flags; 
 	 
 	// set all flags to true other than 0 and 1
 	private void init(boolean[] flags) {
@@ -14,33 +16,53 @@ public class PrimeNumber {
 		}
 	}
 	
-	int[] doSieve(boolean[] flags, int max) {
+	// walk the flags and find the next true
+	private int getNextPrime(int current, int max) {
+	  boolean nextFlag=false;
+	  current +=1; // advance to next
+	  
+	  while ((nextFlag == true) && (current <= max)){
+	     nextFlag = flags[current];
+	     current++;
+	  }
+	  return current;
+	}
+	
+	private int[] doSieve(boolean[] flags, int max) {
 		int current = 2; 
+		int[] primes = new int[max];
+		int primeIndex = 1; 
+		primes[0] = 1;
+		primes[primeIndex] = current;
+		  
 		while (current <= max) {
 			// Set all multiples of this prime to false, 
 			// leave the prime itself true
 			for (int i=2*current; i<max; i+=current) {
 				flags[i] = false; 
 			}
-			
 			current = getNextPrime(current, max); 
+			System.out.println("Current: " + current);
+			primes[primeIndex++] = current;      
 		}
-		
+		return primes; 
 	}
 	
 	public int[] getPrimes(int max) {
-		
-		boolean[] flags = new boolean[max+1]; 
+		flags = new boolean[max+1]; 
 		init(flags);
 		int[] primes = doSieve(flags, max); 
 		return primes; 
 	}
 	
 	public static void main(String[] args) {
-		int max = 20; 
+        System.out.println("Running");
+		int max = 7; 
 		PrimeNumber pn = new PrimeNumber();
 		int[] primes = pn.getPrimes(max); 
-		
+		for (int i=0; i< primes.length; i++) {
+		   System.out.println(primes[i] + " "); 
+		}
 	}
 
 }
